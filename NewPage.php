251af04870646fff5843fbfbfbcb8bd28e5002d4
Jean-Claude Dufourd <jean-claude.dufourd@telecom-paristech.fr>
2017-12-20 16:20:49 +0100

    fix upgrade

--- NewPage.php
+++ NewPage.php
@@ -39,10 +39,9 @@ namespace MPAT\NewPageWizard;
 
 class NewPage
 {
-    function init()
-    {
+    function init() {
         /* we declare that we have some languages files for internationalization */
-        load_plugin_textdomain('mpat-newpage-wizard', false, basename( dirname( __FILE__ ) ) . '/languages' );
+        load_plugin_textdomain('mpat-newpage-wizard', false, basename(dirname(__FILE__)) . '/languages');
 
         /* creating a hidden menu '_doesnotexist' */
         add_submenu_page('_doesntexist', __("Wizard", "mpat-newpage-wizard"), '', 'manage_options', 'MPAT_NewPageWizard', array(&$this, 'load_js'), 'dashicons-welcome-learn-more');
@@ -51,29 +50,32 @@ class NewPage
         add_option('mpatNewPageWizard', '');
 
         /* add this option to general settings overview */
-		add_settings_section(  
-			'wizard_settings_section',
-			__('Wizard', "mpat-newpage-wizard"),
-			function(){
+        add_settings_section(
+            'wizard_settings_section',
+            __('Wizard', "mpat-newpage-wizard"),
+            function () {
                 _e('NewPage Wizard settings', "mpat-newpage-wizard");
             },
-			'general'
-		);
-		add_settings_field(
-			'mpatNewPageWizard',
-			__('Remove "Add-Page" (show only the Wizard button)', "mpat-newpage-wizard"),
-			function($args){
-                $option = get_option($args[0],'');
-                $cmp = strcmp($option , "on");
+            'general'
+        );
+        add_settings_field(
+            'mpatNewPageWizard',
+            __('Remove "Add-Page" (show only the Wizard button)', "mpat-newpage-wizard"),
+            function ($args) {
+                $option = get_option($args[0], '');
+                $cmp = strcmp($option, "on");
                 $chk = ($cmp == 0) ? 'checked' : '';
-                echo '<input type="checkbox" id="'.$args[0].'" name="'.$args[0].'" '.$chk.' />';
+                echo '<input type="checkbox" id="' . $args[0] . '" name="' . $args[0] . '" ' . $chk . ' />';
             },
-			'general',
-			'wizard_settings_section',
-			array('mpatNewPageWizard')
-		); 
-        register_setting('general', 'mpatNewPageWizard','esc_attr');
+            'general',
+            'wizard_settings_section',
+            array('mpatNewPageWizard')
+        );
+        register_setting('general', 'mpatNewPageWizard', 'esc_attr');
+        add_action('admin_footer', array(&$this, 'insert'));
+    }
 
+    function insert() {
         /* check the loop */
         if (isset($_GET['post_type']) && $_GET['post_type'] === 'page') {
             /* remove/replace the 'Add New-button' ? */
@@ -110,12 +112,12 @@ class NewPage
          <?php
         }
     }
-    
+
     function load_js()
     {
         /* we need the wordpress api */
         wp_enqueue_script('wp-api');
-        
+
         /* load the i18n variables */
         wp_register_script('mpat_npw_i18n', null, array("wp-api"));
         wp_localize_script('mpat_npw_i18n', 'npwi18n', array(
@@ -129,6 +131,6 @@ class NewPage
       //wp_enqueue_script('mpat-newpage-wizard', 'http://localhost:8000/rui.js', array('wp-api'), 1.0, true );
     }
 }
- 
+
  $np = new NewPage();
  add_action("admin_menu", array(&$np, "init") );
