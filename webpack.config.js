e750c1dbbe65751bb47df2309db7a283dc8c4518
Jean-Philippe Ruijs <jean-philippe@ruijs.fr>
2019-07-21 00:34:31 +0200

    npm package updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
 var webpack = require('webpack');
 var path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 module.exports = {
   entry: [APP_DIR + '/rui.jsx'],
-  output: {path: BUILD_DIR, filename: 'rui.js'},
+  output: { path: BUILD_DIR, filename: 'rui.js' },
   plugins: [
     new webpack.DefinePlugin(
-        {'process.env': {'NODE_ENV': JSON.stringify('production')}}),
-    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+      { 'process.env': { 'NODE_ENV': JSON.stringify('production') } }),
+    // new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
   ],
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js?/,
       include: APP_DIR,
-      loader: 'babel-loader',
-      query: {presets: ['es2015', 'react']}
+      //loader: 'babel-loader',
+      //query: { presets: ['es2015', 'react'] },
+      use: { loader: "babel-loader" }
     }]
   },
-  stats: {colors: true}
+  stats: { colors: true },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({ /* your config */ })
+    ]
+  }
 };
